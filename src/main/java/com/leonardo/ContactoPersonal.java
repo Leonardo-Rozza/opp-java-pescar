b88355889a0b96112368ccd6adfd5ffa7ac2cb83
Leonardo-Rozza <leonardo155964@gmail.com>
2025-08-28 15:37:19 -0300

    Utilizando construtores

--- src/main/java/com/leonardo/ContactoPersonal.java
+++ src/main/java/com/leonardo/ContactoPersonal.java
@@ -5,8 +5,23 @@ public class ContactoPersonal {
   private String nombre;
   private String correo;
 
+  public ContactoPersonal(String nombre, String correo) {
+    this.nombre = nombre;
+    this.correo = correo;
+  }
+
+  public ContactoPersonal() {
+  }
+
   public void mostrarDatos() {
     System.out.println("El nombre es: " + this.nombre);
   }
 
+  @Override
+  public String toString() {
+    return "ContactoPersonal{" +
+            "nombre='" + nombre + '\'' +
+            ", correo='" + correo + '\'' +
+            '}';
+  }
 }
--- src/main/java/com/leonardo/Test.java
+++ src/main/java/com/leonardo/Test.java
@@ -3,5 +3,13 @@ package com.leonardo;
 public class Test {
   public static void main(String[] args) {
     System.out.println("Hola mundo desde Java");
+
+    ContactoPersonal cp1 = new ContactoPersonal();
+    ContactoPersonal cp2 = new ContactoPersonal("Leo", "Leo@gmail.com");
+
+    // cp1.mostrarDatos();
+
+    System.out.println(cp1);
+    System.out.println(cp2);
   }
 }
