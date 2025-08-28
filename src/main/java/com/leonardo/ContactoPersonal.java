7c58d339f335bbf08d11bab81dab359413f70462
Leonardo-Rozza <leonardo155964@gmail.com>
2025-08-28 15:26:13 -0300

    Agregando Contacto Personal

---
+++ src/main/java/com/leonardo/ContactoPersonal.java
@@ -0,0 +1,12 @@
+package com.leonardo;
+
+public class ContactoPersonal {
+
+  private String nombre;
+  private String correo;
+
+  public void mostrarDatos() {
+    System.out.println("El nombre es: " + this.nombre);
+  }
+
+}
